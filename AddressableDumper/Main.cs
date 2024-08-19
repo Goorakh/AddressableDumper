885cfb25f1c80a20531609c95796a0a953514c05
Gorakh <petter.ohlen@gmail.com>
2024-08-19 18:14:02 +0200

    Remove old testing code

--- AddressableDumper/Main.cs
+++ AddressableDumper/Main.cs
@@ -1,11 +1,8 @@
-using AddressableDumper.ValueDumper.Serialization;
 using BepInEx;
-using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.ResourceLocations;
@@ -75,117 +72,5 @@ namespace AddressableDumper
         {
             return Addressables.LoadResourceLocationsAsync((IEnumerable)keys, mergeMode).WaitForCompletion();
         }
-
-        /*
-        public void WriterTest()
-        {
-            using MemoryStream ms = new MemoryStream();
-            using StreamWriter writer = new StreamWriter(ms, Encoding.UTF8, 2048, true);
-            using JsonWriter jsonWriter = new JsonTextWriter(writer)
-            {
-                Formatting = Formatting.Indented,
-                CloseOutput = false,
-                AutoCompleteOnClose = false,
-            };
-
-            try
-            {
-                WriteOperationBuilder builder = new WriteOperationBuilder(new JsonWriteOperationBuilderWriter(jsonWriter))
-                {
-                    AutoFlushCapacity = int.MaxValue
-                };
-
-                WriteOperationBuilder splitBuilder = new WriteOperationBuilder(null)
-                {
-                    AutoFlushCapacity = int.MaxValue
-                };
-
-                void writeRandomObject(float elementChance, WriteOperationBuilder splitBuilder)
-                {
-                    bool startedObject = false;
-
-                    Xoroshiro128Plus rng = RoR2.RoR2Application.rng;
-
-                    splitBuilder = new WriteOperationBuilder(splitBuilder);
-
-                    void add(WriteOperation operation)
-                    {
-                        builder.Add(operation.Clone());
-                        splitBuilder.Add(operation.Clone());
-                    }
-
-                    while (rng.nextNormalizedFloat < elementChance)
-                    {
-                        elementChance *= 0.75f;
-
-                        if (!startedObject)
-                        {
-                            startedObject = true;
-                            add(WriteOperation.StartObject());
-                        }
-
-                        int propId = rng.RangeInt(1, 1025);
-                        add(WriteOperation.PropertyName($"prop_{propId:X}"));
-
-                        switch (rng.RangeInt(0, 2))
-                        {
-                            case 0:
-                                int value = rng.RangeInt(1, 1025);
-
-                                if (rng.nextNormalizedFloat < 0.3f)
-                                {
-                                    writeRandomObject(elementChance, splitBuilder);
-                                }
-                                else
-                                {
-                                    add(WriteOperation.ValueRaw(value.ToString()));
-                                }
-
-                                break;
-                            case 1:
-                                add(WriteOperation.StartArray());
-
-                                int arrayCount = rng.RangeInt(0, 6);
-                                for (int i = 0; i < arrayCount; i++)
-                                {
-                                    writeRandomObject(elementChance, splitBuilder);
-                                }
-
-                                add(WriteOperation.EndArray());
-                                break;
-                        }
-                    }
-
-                    if (startedObject)
-                    {
-                        add(WriteOperation.EndObject());
-                    }
-                    else
-                    {
-                        add(WriteOperation.Null());
-                    }
-
-                    splitBuilder.Flush();
-                }
-                
-                float elementChance = 1f;
-                writeRandomObject(elementChance, splitBuilder);
-
-                Log.Info($"builder:\n{string.Join("\n", builder)}");
-
-                Log.Info($"split builder:\n{string.Join("\n", splitBuilder)}");
-
-                builder.Flush();
-            }
-            finally
-            {
-                jsonWriter.Flush();
-
-                ms.Position = 0;
-                using StreamReader reader = new StreamReader(ms, Encoding.UTF8, false, 2048, true);
-                Log.Info("\n" + reader.ReadToEnd());
-            }
-        }
-        */
     }
 }
