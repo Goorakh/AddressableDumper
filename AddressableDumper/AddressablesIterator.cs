11b334df1d97685fe08f169afa6ee115e9aff1ef
Gorakh <petter.ohlen@gmail.com>
2024-09-11 00:28:51 +0200

    Dont automatically refresh keys on start

--- AddressableDumper/AddressablesIterator.cs
+++ AddressableDumper/AddressablesIterator.cs
@@ -84,7 +84,7 @@ namespace AddressableDumper
 
             if (refresh)
             {
-                refreshCache();
+                Log.Warning("Cache refresh required");
             }
             else
             {
