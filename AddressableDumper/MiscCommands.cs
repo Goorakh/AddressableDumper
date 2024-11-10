a11f044b15956106cd1d50ee2430f02c4f2c4685
Gorakh <petter.ohlen@gmail.com>
2024-11-10 14:44:47 +0100

    Fix full_dump command

--- AddressableDumper/MiscCommands.cs
+++ AddressableDumper/MiscCommands.cs
@@ -5,7 +5,7 @@ namespace AddressableDumper
     static class MiscCommands
     {
         [ConCommand(commandName = "full_dump")]
-        static void CCFullDump()
+        static void CCFullDump(ConCommandArgs args)
         {
             Console.instance.SubmitCmd(null, "refresh_addressables_key_cache; dump_addressable_values; dump_scenes");
         }
